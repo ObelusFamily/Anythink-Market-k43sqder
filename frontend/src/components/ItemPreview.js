5d8d82d7ca414563acad7b411c47d710480e0439
WilcoApp <hello@wilco.gg>
2023-04-06 05:39:24 +0000

    detect empy img replace with default img

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import agent from "../agent";
 import { connect } from "react-redux";
 import { ITEM_FAVORITED, ITEM_UNFAVORITED } from "../constants/actionTypes";
-
+import defaultImg from "../imgs/placeholder.png"
 const mapDispatchToProps = (dispatch) => ({
+  
   favorite: (slug) =>
     dispatch({
       type: ITEM_FAVORITED,
@@ -19,6 +21,7 @@ const mapDispatchToProps = (dispatch) => ({
 
 const ItemPreview = (props) => {
   const item = props.item;
+  const [img, setImg]=useState(item.image)
 
   const handleClick = (ev) => {
     ev.preventDefault();
@@ -29,6 +32,9 @@ const ItemPreview = (props) => {
     }
   };
 
+  const haneleImgError=()=>setImg(defaultImg)
+  
+
   return (
     <div
       className="card bg-dark border-light p-3"
@@ -37,7 +43,8 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image}
+        onError={haneleImgError}
+        src={img}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
