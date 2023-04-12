8820636791981e164f55968b5fb004bfe501471e
Villy Siu <villysiu@gmail.com>
2023-04-12 04:39:03 +0000

    add fake data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+User.destroy_all
+
+10.times do |index|
+    u=User.create!(
+        username: Faker::Name.first_name,
+        email: Faker::Internet.email,
+        password: Faker::Internet.password(min_length: 8, max_length: 12)
+    )
+    puts u.id 
+    item=Item.create!(
+        title: Faker::TvShows::Friends.character,
+        description: Faker::TvShows::Friends.quote,
+        user_id: u.id,
+        image: Faker::LoremFlickr.image(size: "50x60", search_terms: ['sports']),
+    )
+    Comment.create!(
+        body: Faker::Coffee.notes,
+        user_id:u.id,
+        item_id: item.id,
+    )
+end
+
+
+
+puts "Created #{User.count} users"
+puts "Created #{Item.count} items"
+puts "Created #{Comment.count} comments"
--- backend/seeds.sh
+++ backend/seeds.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-
 bin/rails db:seed
